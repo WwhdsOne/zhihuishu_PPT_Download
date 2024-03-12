b48f219c59181d128d43099c86354460e8896d3a
Wwhds <a1605691832@163.com>
2024-03-12 13:47:05 +0800

    智慧树PPT下载油猴脚本V0.1.0完成,完成了基本功能并成功下载了计组PPT。其他课程PPT下载有待实验

---
+++ 智慧树PPT下载油猴插件.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         智慧树PPT下载
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  try to take over the world!
+// @author       Wwhds
+// @match        https://hike-doc-online-h5.zhihuishu.com/alweb.html?*
+// @icon         https://wwhds-markdown-image.oss-cn-beijing.aliyuncs.com/Markdown%E4%BD%BF%E7%94%A8/w.jpg
+// @grant        none
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/1.10.1/jquery.min.js
+// @run-at       document-end
+// ==/UserScript==
+
+
+(function ($) {
+    $(function () {
+        console.log("当前URL: " + window.document.URL)
+        // 获取网页内容
+        // PPTX资源所在元素
+        function getPPTXSource() {
+            // 使用window.document获取当前网页内容
+            var divs = window.document
+            var pptxUrl = divs.URL
+            var regex = /WOPISrc=(.*)/;
+            var match = pptxUrl.match(regex);
+            console.log("资源地址: " + pptxUrl)
+            if (match) {
+                console.log(match[1]);
+                fetch(match[1])
+                    .then(response => response.blob())
+                    .then(blob => {
+                        // Create a new object URL for the blob
+                        var url = match[1]
+                        // Create a link and click it to start the download
+                        var a = document.createElement('a');
+                        a.href = url;
+                        document.body.appendChild(a); // Required for Firefox
+                        a.click();
+                        a.remove();
+                    })
+                    .catch(e => console.error(e));
+            } else {
+                console.log("No match found.");
+            }
+        }
+
+        if (window.document.URL.indexOf("ppt") != -1 && confirm("确定要下载PPT吗?")){
+            getPPTXSource();
+        }
+    });
+})(jQuery);
